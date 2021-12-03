1b6ed987a37fa23a6e3f4b41492d3b547d1f30cb
Sean <sdillon215@gmail.com>
2021-12-02 21:56:06 -0700

    server set up, addUser/login functioning

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,12 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
-import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Success from './pages/Success';
 import Nav from './components/Nav';
-import { StoreProvider } from "./utils/GlobalState";
-import OrderHistory from './pages/OrderHistory';
+// import { StoreProvider } from "./utils/GlobalState";
+// import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -48,9 +46,7 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route exact path="/success" component={Success} />
+              {/* <Route exact path="/orderHistory" component={OrderHistory} /> */}
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
