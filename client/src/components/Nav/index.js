b5a8dbf957d33e45fcfc4ee61c81e100eada4896
Sean <sdillon215@gmail.com>
2022-03-15 20:57:07 -0700

    navBlur

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -71,18 +71,9 @@ import Typography from '@mui/material/Typography';
 
 export default function Nav(props) {
     return (
-        <Box sx={{ flexGrow: 1, bgcolor: '#018754' }}>
+        <Box class="blurNav">
             <AppBar position="static" class="nav">
                 <Toolbar>
-                    {/* <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton> */}
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
                         Dillon Floral Corporation
                     </Typography>
