d5779484edf340df8daabc0b9cf467004c2ae85a
Sean <sdillon215@gmail.com>
2022-03-17 08:52:24 -0700

    seeds file added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,43 +1,44 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    dept: String
-    username: String
-    firstName: String
-    lastName: String
-    email: String
-    porders: [Porder]
-    sorders: [Sorder]
-  }
 
-  type Product {
-    _id: ID
-    name: String
-    description: String
-    image: String
-    price: Float
-    quantity: Int
-    department: Department
-  }
+type Product {
+  _id: ID
+  name: String
+  description: String
+  image: String
+  price: Float
+  quantity: Int
+  department: Department
+}
 
-  type Department {
-    _id: ID
-    name: String
-  }
+type Department {
+  _id: ID
+  name: String
+}
 
-  type Porder {
-    username: String
-    purchaseDate: String
-    products: [Product]
-  }
+type Porder {
+  username: String
+  purchaseDate: String
+  products: [Product]
+}
 
-  type Sorder {
-    username: String
-    saleDate: String
-    products: [Product]
-  }
+type Sorder {
+  username: String
+  saleDate: String
+  products: [Product]
+}
+
+type User {
+  _id: ID
+  dept: String
+  username: String
+  firstName: String
+  lastName: String
+  email: String
+  porders: [Porder]
+  sorders: [Sorder]
+}
 
   type Auth {
     token: ID
