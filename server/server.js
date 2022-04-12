226b5fa895b008f74ed38e14b1dfe25878a879f9
Sean <sdillon215@gmail.com>
2022-04-11 18:19:20 -0700

    heroku trouble shooooooooting

--- server/server.js
+++ server/server.js
@@ -1,38 +1,48 @@
 const express = require('express');
+// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
+// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = await new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
 
-server.applyMiddleware({ app });
+const startServer = async () => {
+    // create a new Apollo server and pass in our schema data
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: authMiddleware
+    });
+
+    // Start the Apollo server
+    await server.start();
+
+    // integrate our Apollo server with the Express application as middleware
+    server.applyMiddleware({ app });
+};
+
+// Initialize the Apollo server
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
-
+// Serve up static assests
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
 });
